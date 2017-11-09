9ccbc064c9cc7af63e660a6ef937803c4474026a
chovCodeur <michel@lesecuriesdeventadour.com>
2017-11-09 16:19:23 +0100

    TEST MIPA

--- app/src/main/java/com/example/michelparis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/michelparis/myapplication/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        // MA BITE
+        // MA BITE , pas la mienne
     }
 
 }
