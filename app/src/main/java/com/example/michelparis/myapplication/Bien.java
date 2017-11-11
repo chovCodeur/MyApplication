e63b58b22c01bc978aa900ad10ba28e4c64a1d81
keket87 <keket87@homail.fr>
2017-11-11 15:17:25 +0100

    Classes Bien et Liste

---
+++ app/src/main/java/com/example/michelparis/myapplication/Bien.java
@@ -0,0 +1,84 @@
+package com.example.michelparis.myapplication;
+
+/**
+ * Created by Kevin on 11/11/2017.
+ */
+
+public class Bien {
+
+    private int id_bien;
+    private String nom_bien;
+    private String date_saisie_bien;
+    private String date_achat_bien;
+    //private XXXX facture_bien;
+    private String commentaire_bien;
+    private float prix_bien;
+    //private XXXX photo_bien;
+    private int id_categorie_bien;
+
+    public Bien(int id_bien, String nom_bien, String date_saisie_bien, String date_achat_bien, String commentaire_bien, float prix_bien, int id_categorie_bien) {
+        this.id_bien = id_bien;
+        this.nom_bien = nom_bien;
+        this.date_saisie_bien = date_saisie_bien;
+        this.date_achat_bien = date_achat_bien;
+        this.commentaire_bien = commentaire_bien;
+        this.prix_bien = prix_bien;
+        this.id_categorie_bien = id_categorie_bien;
+    }
+
+    public int getId_bien() {
+        return id_bien;
+    }
+
+    public void setId_bien(int id_bien) {
+        this.id_bien = id_bien;
+    }
+
+    public String getNom_bien() {
+        return nom_bien;
+    }
+
+    public void setNom_bien(String nom_bien) {
+        this.nom_bien = nom_bien;
+    }
+
+    public String getDate_saisie_bien() {
+        return date_saisie_bien;
+    }
+
+    public void setDate_saisie_bien(String date_saisie_bien) {
+        this.date_saisie_bien = date_saisie_bien;
+    }
+
+    public String getDate_achat_bien() {
+        return date_achat_bien;
+    }
+
+    public void setDate_achat_bien(String date_achat_bien) {
+        this.date_achat_bien = date_achat_bien;
+    }
+
+    public String getCommentaire_bien() {
+        return commentaire_bien;
+    }
+
+    public void setCommentaire_bien(String commentaire_bien) {
+        this.commentaire_bien = commentaire_bien;
+    }
+
+    public float getPrix_bien() {
+        return prix_bien;
+    }
+
+    public void setPrix_bien(float prix_bien) {
+        this.prix_bien = prix_bien;
+    }
+
+    public int getId_categorie_bien() {
+        return id_categorie_bien;
+    }
+
+    public void setId_categorie_bien(int id_categorie_bien) {
+        this.id_categorie_bien = id_categorie_bien;
+    }
+}
---
+++ app/src/main/java/com/example/michelparis/myapplication/Liste.java
@@ -0,0 +1,41 @@
+package com.example.michelparis.myapplication;
+
+/**
+ * Created by Kevin on 11/11/2017.
+ */
+
+public class Liste {
+    private int id_liste;
+    private String libelle_liste;
+    private String commentaire_liste;
+
+    public Liste(int id_liste, String libelle_liste, String commentaire_liste) {
+        this.id_liste = id_liste;
+        this.libelle_liste = libelle_liste;
+        this.commentaire_liste = commentaire_liste;
+    }
+
+    public int getId_liste() {
+        return id_liste;
+    }
+
+    public void setId_liste(int id_liste) {
+        this.id_liste = id_liste;
+    }
+
+    public String getLibelle_liste() {
+        return libelle_liste;
+    }
+
+    public void setLibelle_liste(String libelle_liste) {
+        this.libelle_liste = libelle_liste;
+    }
+
+    public String getCommentaire_liste() {
+        return commentaire_liste;
+    }
+
+    public void setCommentaire_liste(String commentaire_liste) {
+        this.commentaire_liste = commentaire_liste;
+    }
+}
