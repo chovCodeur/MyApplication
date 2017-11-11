12fe9623eb49988c26848c3915b7eb3a09d2af57
Rodnor <tristan.lascombe@gmail.com>
2017-11-11 15:54:01 +0100

    Ajout du fichier XML activity_infos_bien.xml complété + début de fichier InfosBien.java

---
+++ app/src/main/java/com/example/michelparis/myapplication/InfosBien.java
@@ -0,0 +1,17 @@
+package com.example.michelparis.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rodnor on 11/11/2017.
+ */
+
+public class InfosBien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infos_bien);
+    }
+}
