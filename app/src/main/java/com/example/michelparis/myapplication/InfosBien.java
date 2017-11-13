74420a944e65ea79f319b2f571aeeb8fcc331966
Rodnor <tristan.lascombe@gmail.com>
2017-11-13 09:14:49 +0100

    Implémentation du Drawer dans l'activité InfosBien

--- app/src/main/java/com/example/michelparis/myapplication/InfosBien.java
+++ app/src/main/java/com/example/michelparis/myapplication/InfosBien.java
@@ -1,7 +1,13 @@
 package com.example.michelparis.myapplication;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +16,7 @@ import android.widget.TextView;
  * Created by Rodnor on 11/11/2017.
  */
 
-public class InfosBien extends AppCompatActivity {
+public class InfosBien extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
    // private BienManager bm;
    // private CategorieManager cm;
@@ -25,11 +31,35 @@ public class InfosBien extends AppCompatActivity {
     private TextView prix;
     private TextView numeroSerie;
     private TextView commentaire;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos_bien);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+        toolbarTitle.setTextColor(getResources().getColor(R.color.toolbarTitle));
+
+        myToolbar.setTitle("typuo");
+        setSupportActionBar(myToolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, myToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View headerview = navigationView.getHeaderView(0);
+
+        headerview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
         bien = new Bien(1, "Lunettes de soleil", "12-11-2017", "23-02-2012", "Ce sont des lunettes de soleil Rayban de type Aviator", 120.99f, 1);
 
@@ -92,5 +122,42 @@ public class InfosBien extends AppCompatActivity {
             commentaire = (TextView) findViewById(R.id.commentairesBien);
             commentaire.setText(bien.getCommentaire_bien());
        // }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+       /* if (id == R.id.youtube) {
+            Intent youtube = new Intent(Intent.ACTION_VIEW);
+            youtube.setData(Uri.parse("https://www.youtube.com/user/bibi300"));
+            startActivity(youtube);
+        }
+
+        if (id == R.id.twitch) {
+            Intent twitch = new Intent(Intent.ACTION_VIEW);
+            twitch.setData(Uri.parse("https://go.twitch.tv/bibi300"));
+            startActivity(twitch);
+        }
+
+        if (id == R.id.dailymotion) {
+            Intent dailymotion = new Intent(Intent.ACTION_VIEW);
+            dailymotion.setData(Uri.parse("https://www.dailymotion.com/bibi300"));
+            startActivity(dailymotion);
+        }*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
