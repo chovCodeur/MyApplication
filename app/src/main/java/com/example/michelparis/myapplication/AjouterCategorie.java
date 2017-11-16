f8951144dfdedb5b920d530e6e0e9ca7ef931bde
Tanguy <boligo2009@live.fr>
2017-11-16 13:52:05 +0100

    xml pour catégorie et Mes infos

--- app/src/main/java/com/example/michelparis/myapplication/AjouterCategorie.java
+++ app/src/main/java/com/example/michelparis/myapplication/AjouterCategorie.java
@@ -2,12 +2,31 @@ package com.example.michelparis.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AjouterCategorie extends AppCompatActivity {
+
+
+    Button addCategorie;
+    EditText editCategorie;
+    EditText editDescriptif;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_categorie);
+
+
+        editCategorie = (EditText) findViewById(R.id.editCategorie);
+        editDescriptif = (EditText) findViewById(R.id.editDescriptif);
+
+        addCategorie = (Button) findViewById(R.id.buttonAddCategorie);
+        addCategorie.setOnClickListener(this);
+
+        editCategorie.setText(categorie.categorie);
+        editDescriptif.setText(categorie.descriptif);
+
     }
 }
--- app/src/main/java/com/example/michelparis/myapplication/ModifierPersonne.java
+++ app/src/main/java/com/example/michelparis/myapplication/ModifierPersonne.java
@@ -2,12 +2,46 @@ package com.example.michelparis.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class ModifierPersonne extends AppCompatActivity {
+
+    Button ajoutInfos;
+    EditText editNom;
+    EditText editPrenom;
+    EditText editMail;
+    EditText editPhoneNumber;
+    EditText editAddress;
+    DatePicker datePicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifier_personne);
+
+        editNom = (EditText)findViewById(R.id.editNom);
+        editPrenom = (EditText)findViewById(R.id.editPrenom);
+        editAddress = (EditText)findViewById(R.id.editAdress);
+        editPhoneNumber = (EditText)findViewById(R.id.editPhon_Number);
+        editMail = (EditText)findViewById(R.id.editEmail);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+
+
+        editNom.setText(personne.nom);
+        editPrenom.setText(personne.prenom);
+        editAddress.setText(personne.address);
+        editMail.setText(personne.mail);
+        editPhoneNumber.setText(personne.phoneNumber);
+        datePicker.setOnDateChangedListener(personne.datePicker);
+
+
+
+
+
+        ajoutInfos = (Button) findViewById(R.id.buttonAjoutInfos);
+        ajoutInfos.setOnClickListener(this);
     }
 }
