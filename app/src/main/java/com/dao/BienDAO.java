4337d4df0691c6b08776ddb3331c98a1f2da4ed7
Rodnor <tristan.lascombe@gmail.com>
2017-12-12 12:15:37 +0100

    correction bug requête

--- app/src/main/java/com/dao/BienDAO.java
+++ app/src/main/java/com/dao/BienDAO.java
@@ -162,7 +162,7 @@ public class BienDAO {
     public String getImageBienByNom(String nom){
         String img="";
 
-        Cursor c = db.rawQuery("SELECT * FROM "+TABLE_NAME+" WHERE "+NOM+"='"+nom+"'", null);
+        Cursor c = db.rawQuery("SELECT * FROM "+TABLE_NAME+" WHERE "+NOM+"=\""+nom+"\"", null);
         if (c.moveToFirst()) {
             img = c.getString(c.getColumnIndex(PHOTO_PRINCIPALE));
             c.close();
